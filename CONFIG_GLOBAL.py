613b90eb47476c867c8884ab7c4af5c80b4c1df0
FabianWylczoch <fabian.wylczoch@web.de>
2023-06-13 14:44:54 +0200

    non-simplified data

--- CONFIG_GLOBAL.py
+++ CONFIG_GLOBAL.py
@@ -1,18 +1,32 @@
 class CONFIG_GLOBAL:
-    PATH_ORIGINAL_DATA_FOLDER = r'/Users/stephanernstberger/PycharmProjects/KIPSeminar/original_data/'
-    PATH_CLEANED_DATA_FOLDER = r'/Users/stephanernstberger/PycharmProjects/KIPSeminar/cleaned_data/'
+    PATH_ORIGINAL_DATA_FOLDER = r'/Users/fabian/Coding/Uni/KIPSeminar/original_data/'
+    PATH_CLEANED_DATA_FOLDER = r'/Users/fabian/Coding/Uni/KIPSeminar/cleaned_data/'
 
-    PATH_ORIGINAL_DATA_NO_DEFECT_FOLDER = r'/Users/stephanernstberger/PycharmProjects/KIPSeminar/original_data/KGT_noDefect_simplified/'
-    PATH_ORIGINAL_DATA_PITTING_FOLDER = r'/Users/stephanernstberger/PycharmProjects/KIPSeminar/original_data/KGT_pitting_simplified/'
-    PATH_CLEANED_DATA_NO_DEFECT_FOLDER = r'/Users/stephanernstberger/PycharmProjects/KIPSeminar/cleaned_data/KGT_noDefect_simplified/'
-    PATH_CLEANED_DATA_PITTING_FOLDER = r'/Users/stephanernstberger/PycharmProjects/KIPSeminar/cleaned_data/KGT_pitting_simplified/'
 
-    PATH_MODEL_FOLDER = '/Users/stephanernstberger/PycharmProjects/KIPSeminar/Model'
-    PATH_DEPLOYMENT_FOLDER = '/Users/stephanernstberger/PycharmProjects/KIPSeminar/Deployment'
+    PATH_ORIGINAL_DATA_NO_DEFECT_FOLDER_SIMPLIFIED = r'/Users/fabian/Coding/Uni/KIPSeminar/original_data/KGT_noDefect_simplified/'
+    PATH_ORIGINAL_DATA_PITTING_FOLDER_SIMPLIFIED = r'/Users/fabian/Coding/Uni/KIPSeminar/original_data/KGT_pitting_simplified/'
+
+    PATH_CLEANED_DATA_NO_DEFECT_FOLDER_SIMPLIFIED = r'/Users/fabian/Coding/Uni/KIPSeminar/cleaned_data/KGT_noDefect_simplified/'
+    PATH_CLEANED_DATA_PITTING_FOLDER_SIMPLIFIED = r'/Users/fabian/Coding/Uni/KIPSeminar/cleaned_data/KGT_pitting_simplified/'
+
+
+    PATH_ORIGINAL_DATA_NO_DEFECT_FOLDER = r'/Users/fabian/Coding/Uni/KIPSeminar/original_data/KGT_noDefect/'
+    PATH_ORIGINAL_DATA_PITTING_FOLDER = r'/Users/fabian/Coding/Uni/KIPSeminar/original_data/KGT_pitting/'
+
+    PATH_CLEANED_DATA_NO_DEFECT_FOLDER = r'/Users/fabian/Coding/Uni/KIPSeminar/cleaned_data/KGT_noDefect/'
+    PATH_CLEANED_DATA_PITTING_FOLDER = r'/Users/fabian/Coding/Uni/KIPSeminar/cleaned_data/KGT_pitting'
+
+
+    PATH_MODEL_FOLDER = '/Users/fabian/Coding/Uni/KIPSeminar/Model'
+    PATH_DEPLOYMENT_FOLDER = '/Users/fabian/Coding/Uni/KIPSeminar/Deployment'
     PATH_VIDEO = PATH_DEPLOYMENT_FOLDER + '/pitting_video.mp4'
 
-    PATH_DICT_ORIGINAL = {'no_defect': PATH_ORIGINAL_DATA_NO_DEFECT_FOLDER,
+    PATH_DICT_ORIGINAL = {'no_defect_simplified': PATH_ORIGINAL_DATA_NO_DEFECT_FOLDER_SIMPLIFIED,
+                          'pitting_simplified': PATH_ORIGINAL_DATA_PITTING_FOLDER_SIMPLIFIED,
+                          'no_defect': PATH_ORIGINAL_DATA_NO_DEFECT_FOLDER,
                           'pitting': PATH_ORIGINAL_DATA_PITTING_FOLDER}
 
-    PATH_DICT_CLEANED = {'no_defect': PATH_CLEANED_DATA_NO_DEFECT_FOLDER,
+    PATH_DICT_CLEANED = {'no_defect_simplified': PATH_CLEANED_DATA_NO_DEFECT_FOLDER_SIMPLIFIED,
+                         'pitting_simplified': PATH_CLEANED_DATA_PITTING_FOLDER_SIMPLIFIED,
+                         'no_defect': PATH_CLEANED_DATA_NO_DEFECT_FOLDER,
                          'pitting': PATH_CLEANED_DATA_PITTING_FOLDER}
