c14048d552e928b3b953e0a98778be67e3238006
Stephan1324 <92784492+Stephan1324@users.noreply.github.com>
2023-06-13 13:37:20 +0200

    Augmentation und Grid Search implementiert

--- main.py
+++ main.py
@@ -9,14 +9,15 @@ if __name__ == "__main__":
     # erster Ansatz mit baseline_model zum trainieren
     # mit model_type='resnet_50' kann weiteres model aus dem internet trainiert werden
 
-    # model_instance = Model(model_type='resnet_18', epochs=50, batch_size=32)
-    # model_instance.split(test_size=0.2, normalize=True, hsv=True)
-    # model_instance.model_building()
-    # model_instance.fit(online_augmentation=True)
-    # model_instance.evaluate()
-    # model_instance.predict_image(img_number=60, hsv=True)
+    model_instance = Model(model_type='baseline', epochs=1, batch_size=32)
+    model_instance.split(test_size=0.2, normalize=True, hsv=True)
+    model_instance.model_building()
+    # model_instance.grid_search(epochs=[1, 2], batch_size=[32])
+    model_instance.fit(online_augmentation=True, augmentation_factor=4)
+    model_instance.evaluate()
+    model_instance.predict_image(img_number=60, hsv=True)
 
 
     # deployment auf dem Video
-    deployment = Video_Deployment(model_type='resnet_18')
+    deployment = Video_Deployment(model_type='baseline')
     deployment.predict(normalize=True, hsv=True)
