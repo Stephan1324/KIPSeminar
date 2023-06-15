23b125496feaa2fc184bcd3ef06bf15477358737
Stephan1324 <92784492+Stephan1324@users.noreply.github.com>
2023-06-15 16:57:46 +0200

    main aktualisierung

--- main.py
+++ main.py
@@ -28,22 +28,22 @@ if __name__ == "__main__":
 
 
     #################Stephan
-        # Data Cleaning für pitting und no_defect Datensätze
-        # Data_cleaning.clean_data('pitting')
-        # Data_cleaning.clean_data('no_defect')
+    # Data Cleaning für pitting und no_defect Datensätze
+    # Data_cleaning.clean_data('pitting')
+    # Data_cleaning.clean_data('no_defect')
 
-        # erster Ansatz mit baseline_model zum Trainieren
-        # mit model_type='resnet_50' kann weiteres model aus dem internet trainiert werden
+    # erster Ansatz mit baseline_model zum Trainieren
+    # mit model_type='resnet_50' kann weiteres model aus dem internet trainiert werden
 
-        # model_instance = Model(model_type='baseline', epochs=10, batch_size=16, initial_learningrate=0.01)
-        # model_instance.split(test_size=0.2, normalize=True, hsv=True)
-        # model_instance.model_building()
-        # # model_instance.grid_search(augmentation_factor=3, epochs=[5, 10], batch_size=[32, 16], learning_rates=[0.001, 0.01])
-        # model_instance.fit(augmentation=True, augmentation_factor=4)
-        # model_instance.evaluate()
-        # model_instance.predict_image(img_number=60, hsv=True)
+    # model_instance = Model(model_type='baseline', epochs=10, batch_size=16, initial_learningrate=0.01)
+    # model_instance.split(test_size=0.2, normalize=True, hsv=True)
+    # model_instance.model_building()
+    # # model_instance.grid_search(augmentation_factor=3, epochs=[5, 10], batch_size=[32, 16], learning_rates=[0.001, 0.01])
+    # model_instance.fit(augmentation=True, augmentation_factor=4)
+    # model_instance.evaluate()
+    # model_instance.predict_image(img_number=60, hsv=True)
 
-        # deployment auf dem Video
-        deployment = Video_Deployment(model_type='baseline', epochs=10, batch_size=16, learning_rate=0.01)
-        deployment.predict(normalize=True, hsv=True)
+    # deployment auf dem Video
+    deployment = Video_Deployment(model_type='baseline', epochs=10, batch_size=16, learning_rate=0.01)
+    deployment.predict(normalize=True, hsv=True)
 
